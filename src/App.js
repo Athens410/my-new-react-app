b016b58a836fc43dab44c005b9ee5d69235b824e
Athens410 <athenah.coucean@gmail.com>
2023-05-24 13:27:01 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './logo.svg';
 import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>testing</h1>
+        
        
         
       </header>
